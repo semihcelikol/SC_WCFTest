dd1b98d78448fb160659ed75711fa97eb88ab5fd
Semih Çelikol <semihcelikol@outlook.com>
2024-11-21 19:26:19 +0300

    Add project files.

---
+++ ServiceProducts/IProductService.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace ServiceProducts
+{
+    [ServiceContract]
+    public interface IProductService
+    {
+        [OperationContract]
+        Product GetProductById(int id);
+    }
+
+    [DataContract]
+    public class Product
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string Description { get; set; }
+
+        public int Qty { get; set; }
+    }
+}
---
+++ ServiceProducts/ProductService.svc.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ServiceProducts
+{
+    public class ProductService : IProductService
+    {
+        private List<Product> GetProductList()
+        {
+            List<Product> products = new List<Product>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Product product = new Product();
+
+                product.Id = i;
+                product.Name = $"Product - {i}";
+                product.Description = $"Product - {i} Description";
+                product.Qty = i + i;
+
+                products.Add(product);
+            }
+
+            return products;
+        }
+
+        public Product GetProductById(int id)
+        {
+            List<Product> products = GetProductList();
+
+            if (products.Exists(x => x.Id == id))
+            {
+                return products.FirstOrDefault(x => x.Id == id);
+            }
+
+            throw new System.Exception("Ürün bulunamadı !");
+        }
+    }
+
+}
