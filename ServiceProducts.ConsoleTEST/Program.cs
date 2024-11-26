b06b206f7fb153878a52f220132d677c9101f7c3
Semih Çelikol <semihcelikol@outlook.com>
2024-11-26 18:07:33 +0300

    WCF Test console.

---
+++ ServiceProducts.ConsoleTEST/Program.cs
@@ -0,0 +1,33 @@
+﻿using WCFProductService;
+
+namespace ServiceProducts.ConsoleTEST
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            try
+            {
+                Product product = null;
+
+                using (ProductServiceClient client = new ProductServiceClient(ProductServiceClient.EndpointConfiguration.BasicHttpBinding_IProductService))
+                {
+                    product = await client.GetProductByIdAsync(5);
+                }
+
+                if (product != null)
+                {
+                    Console.WriteLine($"Product found: {product.Id} - {product.Name} - {product.Description}");
+                }
+
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                Console.ReadKey();
+            }
+        }
+    }
+}
